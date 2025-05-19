6b1c1d63f13d715fb3ec0ccb6a4d93d43ad0f776
vishalthakur06 <vishalultralegend612@gmail.com>
2025-05-19 14:04:53 +0530

    Initial commit

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,23 +1,36 @@
-import React, { useContext } from 'react'
-import {Context} from "../../main"
-import {Link} from "react-router-dom"
-import { FaGithub , FaLinkedin} from "react-icons/fa"
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { Link } from "react-router-dom";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
-import { RiInstagramFill} from "react-icons/ri"
+import { RiInstagramFill } from "react-icons/ri";
 function Footer() {
-  const {isAuthorized}  = useContext(Context)
+  const { isAuthorized } = useContext(Context);
   return (
-    <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
-<div>&copy; All Rights Reserved by Abhishek.</div>
-<div>
-  <Link to={'https://github.com/exclusiveabhi'} target='github'><FaGithub></FaGithub></Link>
-  <Link to={'https://leetcode.com/u/exclusiveabhi/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
-  <Link to={'https://www.linkedin.com/in/abhishek-rajput-/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
-  <Link to={'https://www.instagram.com/exclusiveabhi/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
-</div>
-      
+    <footer className={isAuthorized ? "footerShow" : "footerHide"}>
+      <div>&copy; All Rights Reserved by Vishal.</div>
+      <div>
+        <Link to={"https://github.com/Vishalthakur06"} target="github">
+          <FaGithub></FaGithub>
+        </Link>
+        <Link to={"https://leetcode.com/u/Thakurji-06/"} target="leetcode">
+          <SiLeetcode></SiLeetcode>
+        </Link>
+        <Link
+          to={"https://www.linkedin.com/in/vishal--thakur/"}
+          target="linkedin"
+        >
+          <FaLinkedin></FaLinkedin>
+        </Link>
+        <Link
+          to={"https://www.instagram.com/vishalthakur_06/"}
+          target="instagram"
+        >
+          <RiInstagramFill></RiInstagramFill>
+        </Link>
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
